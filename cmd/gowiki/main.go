949fe3d5ae8ec882a075544748a957afbf1e5aee
Erkhan Burkit <erkhanberkut@Erkhans-MacBook-Pro.local>
2026-08-29 12:39:57 +0500

    without walk dir

--- cmd/gowiki/main.go
+++ cmd/gowiki/main.go
@@ -2,15 +2,49 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"github.com/yuin/goldmark"
+	"path/filepath"
+	"strings"
 )
 
 func main() {
-	data, err := os.ReadFile("content/hello.md")
-	if err != nil {
-		fmt.Println("error:", err)
-		return
-	}
 
-	fmt.Println(string(data))
+	dirPath := "internal/content"
+	files, err := os.ReadDir(dirPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, entry := range files {
+		if entry.IsDir() {
+			continue
+		}
+		if filepath.Ext(entry.Name()) != ".md" {
+				continue
+		}
+		fullPath := filepath.Join(dirPath, entry.Name())
+		content, err := os.ReadFile(fullPath)
+		if err != nil {
+			fmt.Printf("Error reading file %s: %v\n", entry.Name(), err)
+			continue
+		}
+		name := strings.TrimSuffix(entry.Name(), ".md")
+		outputPath := filepath.Join("public", name+".html")
+
+		file, err := os.Create(outputPath)
+		if err != nil {
+			log.Printf("Error creating %s: %v\n", outputPath, err)
+			continue
+		}
+		err = goldmark.Convert(content, file)
+		if err != nil {
+			log.Printf("Error converting %s: %v\n", entry.Name(), err)
+		}
+
+		file.Close()
+	}	
 }
+
+
+
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module wiki 
+module wiki
 
 go 1.24.6
+
+require github.com/yuin/goldmark v1.8.5 // indirect
